5d177af0b386a9e82ffcbba04f377a07b77f382f
mingyip <fypcheung@gmail.com>
2022-03-19 23:01:35 +0000

    audio, rotary & rfid pkg

--- install.sh
+++ install.sh
@@ -4,5 +4,12 @@
 sudo apt-get update
 sudo apt-get install elpa-magit
 sudo apt install git-all -y
+sudo apt install ffmpeg -y
+sudo apt-get install alsa-base alsa-utils -y
+sudo apt install python3-pip -y
 
-
+# pip install python packages
+sudo pip install pyalsaaudio
+sudo pip install spi
+sudo pip install spidev
+sudo pip install mfrc522
